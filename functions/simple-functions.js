3ebbc5ae3f481e0c1a5f3d7fc8b692a25ed311c0
Nicholas Grinage <ngrinage13@gmail.com>
2023-10-23 14:20:54 -0400

    exercise 1

---
+++ functions/simple-functions.js
@@ -0,0 +1,6 @@
+function favoriteThings() {}
+console.log("Nicholas " + "Interstellar " + "Drake");
+function whyImHere() {}
+console.log("To learn how to code");
+function favortiePlace() {}
+console.log("Heinz Field");
