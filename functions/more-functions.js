8ea4deb831ac1e3d0ac061f2ead9a3beea68c748
Nicholas Grinage <ngrinage13@gmail.com>
2023-10-24 11:12:55 -0400

    page 16 exercise 1

---
+++ functions/more-functions.js
@@ -0,0 +1,36 @@
+function displayMailingLabel(name, address, city, state, zip) {
+  console.log("\n", name, "\n", address, "\n", city, "\n", state, "\n", zip);
+}
+
+displayMailingLabel("Nicholas", "500 Grant St", "Pittsburgh", "PA", "15219");
+
+console.log();
+
+function addNumbers(num1, num2) {
+  console.log(num1 + num2);
+}
+
+addNumbers(2, 5);
+
+console.log();
+
+function displayReceipt(totalDue, amountPaid) {
+  let changeDue = amountPaid - totalDue;
+  console.log("Total Due: $ " + totalDue);
+  console.log("Amount Paid: $" + amountPaid);
+  console.log("Change Due: $" + changeDue);
+}
+
+displayReceipt(250, 200);
+
+console.log();
+
+  
+  displayReceipt(200, 250);
+  
+  console.log();
+
+  
+  displayReceipt(200, 200);
+  
+  console.log();
