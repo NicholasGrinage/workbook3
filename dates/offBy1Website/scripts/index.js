d6ab609d510b55d6fce2d9f62a8738efd4579e50
Nicholas Grinage <ngrinage13@gmail.com>
2023-10-27 10:17:49 -0400

    checkbox exercise

--- dates/offBy1Website/scripts/index.js
+++ dates/offBy1Website/scripts/index.js
@@ -1,7 +1,7 @@
 function displayDate() {
-let date = document.getElementById("dateBox")
+  let date = document.getElementById("dateBox");
   let d = new Date(date.value);
   console.log(d.toString());
-    let dateParagraph = document.getElementById("dateParagraph")
-    dateParagraph.innerText = d.toString()
+  let dateParagraph = document.getElementById("dateParagraph");
+  dateParagraph.innerText = d.toString();
 }
